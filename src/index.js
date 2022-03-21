3a9513ad9b32f8fbd9976bf4bcde1e01ba4d22b6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-21 19:44:47 +0500

    add webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  // eslint-disable-next-line no-undef
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
